5a6fb094e472a6a3f8c3e92d61e078c2b43babb2
m <mattniederhuber@gmail.com>
2025-09-12 14:03:01 -0400

    added transpose and variant id colnames to output parquet; added outdir arg

--- model/genotype_loader.py
+++ model/genotype_loader.py
@@ -1,76 +1,12 @@
-#import glow
-#from pyspark.sql import SparkSession
 import polars as pl
 import re
 import torch
 import numpy as np
 
-def loader1_spark():
-    spark = (
-        SparkSession.builder
-        .appName("VCF Loader")
-        # Tell Spark to pull in the Glow JAR
-        .config("spark.jars.packages", "io.projectglow:glow-spark3_2.12:2.0.0")
-        # Required so Glow can read/write BGZF-compressed files (VCF bgz/gz)
-        .config("spark.hadoop.io.compression.codecs", "io.projectglow.sql.util.BGZFCodec")
-        .getOrCreate()
-    )
-
-    # Register Glow functions with Spark
-    glow.register(spark)
-
-    vcf_file = "PGS001990/ukb_imp_bct_vars_merged_effects.vcf"
-
-    # Example: load VCF
-    df = (
-        spark.read.format("vcf")
-        .option("includeSampleIds", True)
-        .load(vcf_file)
-    )
-
-
-    #vcf_reader = vcfpy.Reader.from_path(vcf)
-
-    #samples = vcf_reader.header.samples.names
-    ## for testing
-    #samples = samples[0:5]
-    #print(samples)
-
-    #sample_to_genotypes = { samp: {} for samp in samples }
-
-    #print(sample_to_genotypes)
-
-    #i = 0
-    #for record in vcf_reader:
-            ## Determine an identifier for the variant
-            ## If record.ID is set and not “.”, use that; else make one
-        #varid = record.ID
-        #print(varid)
-        #for call in record.calls[0:5]:
-            #samp = call.sample
-            ## call.data is an OrderedDict containing FORMAT fields
-            ## The genotype is under key “GT”
-            #gt = call.data.get('GT')
-            #print(samp, gt)
-
-            #if gt == '0/0':
-                #gt_score = 0
-            #elif gt == '0/1':
-                #gt_score = 1
-            #elif gt == '1/1':
-                #gt_score = 2
-            #else:
-                #print(f'{gt} ??')
-
-            #sample_to_genotypes[samp][varid[0]] = gt_score
-
-        #if i == 4:
-            #print(sample_to_genotypes)
-            #exit()
-        #i += 1
-        
-def loader1_polars():
-    with open("../../../ukb_for_pgs/ukb_imp_bct_vars_merged_effects.vcf") as f:
+#TODO add test for duplicate family ids before droping subfamily id -- look for duplicates in family ids        
+def loader1_polars(vcf_file, outdir):
+    #with open("../../../ukb_for_pgs/ukb_imp_bct_vars_merged_effects.vcf") as f:
+    with open(vcf_file) as f:
         for line in f:
             if line.startswith("#CHROM"):
                 header = line.lstrip("#").strip().split("\t")
@@ -78,7 +14,8 @@ def loader1_polars():
             
     # Load the data with the fixed header
     df = pl.scan_csv(
-        "../../../ukb_for_pgs/ukb_imp_bct_vars_merged_effects.vcf",
+        #"../../../ukb_for_pgs/ukb_imp_bct_vars_merged_effects.vcf",
+        vcf_file,
         has_header=False,
         new_columns=header,
         separator="\t",
@@ -109,16 +46,22 @@ def loader1_polars():
     encoded = (left == 48).astype(np.int8) + (right == 48).astype(np.int8)
 
     encoded = encoded.reshape(arr.shape)
-    
+
+    ## transpose so variants are columns
+    encoded_transposed = encoded.T
+    dfgeno = pl.DataFrame(encoded_transposed)
+    ## set column names to variant ids for genotype mapping
+    dfgeno.columns = df.select('ID').collect().to_series().to_list()
     # Store in parquet format for further preprocessing
-    dfgeno = pl.DataFrame(encoded)  # or pl.from_numpy(encoded)
-    dfgeno.write_parquet("genotypes.parquet", compression="zstd")
+    dfgeno.write_parquet(f"{outdir}/genotypes.parquet", compression="zstd")
 
     # Store in tensors for training
     t = torch.from_numpy(encoded).to(torch.int8)  # values -1,0,1,2 fit in int8
-    torch.save(t, "genotypes.pt")
+    torch.save(t, f"{outdir}/genotypes.pt")
 
 
-    
 if __name__ == "__main__":
-    loader1_polars()
+
+    vcf_file = "PGS001990/ukb_imp_bct_vars_merged_effects.vcf"
+    loader1_polars(vcf_file, outdir = "PGS001990")
+#    map_variant(annotation_file)
